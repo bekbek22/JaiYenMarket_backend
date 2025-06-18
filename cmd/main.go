8f907c5a966d6c301097d1ffbd53b4ce3d967eb9
bekbek22 <bekbeknakub@gmail.com>
2025-06-18 23:30:47 +0700

    setup project

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world!!")
+	})
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	app.Listen(":3000")
+}
